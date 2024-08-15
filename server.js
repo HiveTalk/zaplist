1f4c3d379d300f512083378b60679f2765807756
TheWildHustle <145122833+TheWildHustle@users.noreply.github.com>
2024-08-15 14:25:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
+const sharp = require('sharp')
 const app = express()
 
 // Serve the index.html file at the root URL
@@ -12,7 +13,14 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(express.json({ limit: '50mb' })) // Increase the limit if necessary
 
-app.post('/save-image', (req, res) => {
+// Function to convert image to PNG format
+const convertToPNG = async (buffer) => {
+  return await sharp(buffer)
+    .png()
+    .toBuffer()
+}
+
+app.post('/save-image', async (req, res) => {
   const { filePath, buffer, pubkey } = req.body
   const dir = path.dirname(filePath)
 
@@ -20,21 +28,27 @@ app.post('/save-image', (req, res) => {
     fs.mkdirSync(dir, { recursive: true })
   }
 
-  fs.writeFile(filePath, Buffer.from(buffer), (err) => {
-    if (err) {
-      console.error(`Failed to save image for ${pubkey}:`, err)
-      return res.status(500).send('Failed to save image')
-    }
+  try {
+    const pngBuffer = await convertToPNG(Buffer.from(buffer))
+    const pngFilePath = filePath.replace(/\.[^/.]+$/, ".png")
 
-    res.send('Image saved successfully')
-  })
+    fs.writeFile(pngFilePath, pngBuffer, (err) => {
+      if (err) {
+        console.error(`Failed to save image for ${pubkey}:`, err)
+        return res.status(500).send('Failed to save image')
+      }
+
+      res.send('Image saved successfully as PNG')
+    })
+  } catch (error) {
+    console.error(`Failed to convert image for ${pubkey}:`, error)
+    res.status(500).send('Failed to convert and save image')
+  }
 })
 
 app.use('/imgstash', express.static(path.join(__dirname, 'imgstash')))
-
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
-
